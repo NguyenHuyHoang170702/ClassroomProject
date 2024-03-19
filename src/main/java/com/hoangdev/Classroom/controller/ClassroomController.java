f845b71e9a4f07fe2d7462f4e49cc2339b8cc01e
011223345aA <huyhoang11202@gmail.com>
2024-03-19 08:35:03 +0700

    add layout, index page

---
+++ src/main/java/com/hoangdev/Classroom/controller/ClassroomController.java
@@ -0,0 +1,4 @@
+package com.hoangdev.Classroom.controller;
+
+public class ClassroomController {
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,53 +1,52 @@
-INSERT INTO users (username, name, email, password, status)
-VALUES ('admin', 'John Doe', 'john@example.com', '$2a$10$gqHrslMttQWSsDSVRTK1OehkkBiXsJ/a4z2OURU./dizwOQu5Lovu', true),
-       ('ramesh', 'Jane Smith', 'jane@example.com', '$2a$10$5PiyN0MsG0y886d8xWXtwuLXK0Y7zZwcN5xm82b4oDSVr7yF0O6em', true),
-       ('mike_wilson', 'Mike Wilson', 'mike@example.com', 'pass456', true);
+-- INSERT INTO users (username, name, email, password, status)
+-- VALUES ('admin', 'John Doe', 'john@example.com', '$2a$10$gqHrslMttQWSsDSVRTK1OehkkBiXsJ/a4z2OURU./dizwOQu5Lovu', true),
+--        ('ramesh', 'Jane Smith', 'jane@example.com', '$2a$10$5PiyN0MsG0y886d8xWXtwuLXK0Y7zZwcN5xm82b4oDSVr7yF0O6em', true),
+--        ('mike_wilson', 'Mike Wilson', 'mike@example.com', 'pass456', true);
+--
+--
+-- INSERT INTO roles (rolename, description)
+-- VALUES ('ROLE_ADMIN', 'Administrator role with full access'),
+--        ('ROLE_STUDENT', 'Regular user role with basic access'),
+--        ('ROLE_TEACHER', 'Teacher role with special access');
+--
+-- INSERT INTO user_role (username, role_id)
+-- VALUES ('admin', 1),  -- Assigning ROLE_ADMIN to user john_doe
+--        ('ramesh', 2), -- Assigning ROLE_USER to user jane_smith
+--        ('mike_wilson', 3); -- Assigning ROLE_TEACHER to user mike_wilson
+--
+--
+-- INSERT INTO classrooms (name_class, description_class, code_class)
+-- VALUES ('Mathematics', 'Classroom for mathematics course.', 'MATH101'),
+--        ('History', 'Classroom for history course.', 'HIST202'),
+--        ('Science', 'Classroom for science course.', 'SCI303');
 
+--
+-- INSERT INTO news (title, content, timestamp, class_id)
+-- VALUES ('Important Announcement', 'Lorem ipsum dolor sit amet.', '2024-03-18 08:00:00', 1),
+--        ('Upcoming Event', 'Consectetur adipiscing elit.', '2024-03-19 10:00:00', 2),
+--        ('Classroom Updates', 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2024-03-20 12:00:00', 3);
 
-INSERT INTO roles (rolename, description)
-VALUES ('ROLE_ADMIN', 'Administrator role with full access'),
-       ('ROLE_STUDENT', 'Regular user role with basic access'),
-       ('ROLE_TEACHER', 'Teacher role with special access');
-
-INSERT INTO user_role (username, role_id)
-VALUES ('admin', 1),  -- Assigning ROLE_ADMIN to user john_doe
-       ('ramesh', 2), -- Assigning ROLE_USER to user jane_smith
-       ('mike_wilson', 3); -- Assigning ROLE_TEACHER to user mike_wilson
-
-
-INSERT INTO classrooms (name_class, description_class, code_class)
-VALUES ('Mathematics', 'Classroom for mathematics course.', 'MATH101'),
-       ('History', 'Classroom for history course.', 'HIST202'),
-       ('Science', 'Classroom for science course.', 'SCI303');
-
-
-INSERT INTO news (title, content, timestamp, class_id)
-VALUES ('Important Announcement', 'Lorem ipsum dolor sit amet.', '2024-03-18 08:00:00', 1),
-       ('Upcoming Event', 'Consectetur adipiscing elit.', '2024-03-19 10:00:00', 2),
-       ('Classroom Updates', 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2024-03-20 12:00:00', 3);
-
-
-INSERT INTO comments (content, timestamp, username, news_id)
-VALUES ('Great news! Looking forward to it.', '2024-03-18 08:30:00', 'john_doe', 1),
-       ('I have some suggestions for improvement.', '2024-03-19 09:00:00', 'jane_smith', 2),
-       ('Can we discuss this further in class?', '2024-03-20 10:30:00', 'mike_wilson', 3);
-
-
-INSERT INTO homeworks (class_id, name, description, size, score, homework_code, content)
-VALUES (1, 'Assignment 1', 'Complete the assigned tasks.', 1024, 80, 'HW001', <binary_data>),
-       (2, 'Project Submission', 'Submit your project by the deadline.', 2048, 90, 'HW002', <binary_data>),
-       (3, 'Research Paper', 'Write a research paper on the given topic.', 4096, 85, 'HW003', <binary_data>);
-
-
-INSERT INTO user_classroom (username, classroom_id)
-VALUES ('ramesh', 1),  -- Assigning classroom Mathematics to user john_doe
-       ('mike_wilson', 3); -- Assigning classroom Science to user mike_wilson
-
-
-INSERT INTO news_user (news_id, username)
-VALUES (1, 'ramesh'),  -- Assigning news ID 1 to user john_doe
-       (3, 'mike_wilson'); -- Assigning news ID 3 to user mike_wilson
-
-INSERT INTO user_homework (username, homework_id)
-VALUES ('ramesh', 1),  -- Assigning homework ID 1 to user john_doe
-       ('mike_wilson', 3); -- Assigning homework ID 3 to user mike_wilson
+--
+-- INSERT INTO comments (content, timestamp, username, news_id)
+-- VALUES ('Great news! Looking forward to it.', '2024-03-18 08:30:00', 'ramesh', 1),
+--        ('Can we discuss this further in class?', '2024-03-20 10:30:00', 'mike_wilson', 3);
+--
+--
+-- INSERT INTO homeworks (class_id, name, description, size, score, homework_code, content)
+-- VALUES (1, 'Assignment 1', 'Complete the assigned tasks.', 1024, 80, 'HW001', null),
+--        (2, 'Project Submission', 'Submit your project by the deadline.', 2048, 90, 'HW002', null),
+--        (3, 'Research Paper', 'Write a research paper on the given topic.', 4096, 85, 'HW003', null);
+--
+--
+-- INSERT INTO user_classroom (username, classroom_id)
+-- VALUES ('ramesh', 1),  -- Assigning classroom Mathematics to user john_doe
+--        ('mike_wilson', 3); -- Assigning classroom Science to user mike_wilson
+--
+--
+-- INSERT INTO news_user (news_id, username)
+-- VALUES (1, 'ramesh'),  -- Assigning news ID 1 to user john_doe
+--        (3, 'mike_wilson'); -- Assigning news ID 3 to user mike_wilson
+--
+-- INSERT INTO user_homework (username, homework_id)
+-- VALUES ('ramesh', 1),  -- Assigning homework ID 1 to user john_doe
+--        ('mike_wilson', 3); -- Assigning homework ID 3 to user mike_wilson
